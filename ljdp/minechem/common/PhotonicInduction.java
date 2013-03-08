e3e31488b0df7ad2843623a4eb9c048a867d7787
MFernflower <machacker9000@gmail.com>
2013-03-08 14:23:12 -0500

    Thanks IC2!!!

---
+++ ljdp/minechem/common/PhotonicInduction.java
@@ -0,0 +1,36 @@
+   // I learned this method from IC2. All credits to the IC2 Team!
+   // public static final EnumToolMaterial IRON
+   package ljdp.minechem.common
+   import net.minecraft.item.EnumToolMaterial;
+   import net.minecraft.item.ItemPickaxe;
+   import net.minecraft.item.ItemStack;
+   import ljdp.minechem.common.ModMinechem;
+   
+   public class PhotonicInduction extends ItemPickaxe
+   {
+     private final ItemStack repairMaterial;
+     public float efficiencyOnProperMaterial;
+   
+     public PhotonicInduction (int id, int index, EnumToolMaterial enumtoolmaterial, float efficiency, ItemStack repairMaterial)
+     {
+     super(id, enumtoolmaterial);
+     this.efficiencyOnProperMaterial = efficiency;
+     setIconIndex(index);
+     this.repairMaterial = repairMaterial;
+     }
+   
+     public String getTextureFile()
+     {
+     return ModMinechem.proxy.BLOCKS_PNG;
+     }
+   
+     public int getItemEnrchantability()
+     {
+     return 8;
+     }
+   
+     public boolean getIsRepairable(ItemStack stack1, ItemStack stack2)
+     {
+     return (stack2 != null) && (stack2.itemID == this.repairMaterial.itemID);
+     }
+   }
