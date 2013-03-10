81875a9657a05a792886699fffe4d198f40998ba
MFernflower <machacker9000@gmail.com>
2013-03-09 20:53:45 -0500

    All machines are now extract only Input is too bugggy

--- ljdp/minechem/common/tileentity/TileEntityDecomposer.java
+++ ljdp/minechem/common/tileentity/TileEntityDecomposer.java
@@ -248,19 +248,8 @@ IPowerReceptor, ITriggerProvider, IMinechemTriggerProvider, ISpecialInventory, I
 
 	@Override
 	public int addItem(ItemStack incoming, boolean doAdd, ForgeDirection from) {
-		
-		if (incoming != null){
-			 if (incoming.itemID == MinechemItems.testTube.itemID) {
-		        	return testTubeTransactor.add(incoming, doAdd).stackSize;
-		        } else {
-		        	ItemStack tmp = inputTransactor.add(incoming, doAdd); return (tmp == null) ? 0 : tmp.stackSize;
-		        }	
-			
-		}else{
-       
+	return 0; 
 	}
-		return kEmptyBottleSlotsSize;
-		}
 
 	@Override
 	public ItemStack[] extractItem(boolean doRemove, ForgeDirection from, int maxItemCount) {
